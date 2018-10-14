215d69346f35dee95009352d6d9f11fdec29ae8d
yago <yagt16@gmail.com>
2018-10-14 17:01:13 +0200

    Dibujo de un triangulo en base a sus vertices

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,65 @@
 #[macro_use]
 extern crate glium;
 
+/**
+    Tutorial obtenido de https://github.com/glium/glium/tree/master/book
+**/
+
 fn main() {
     println!("Welcome to opengl glium");
 
     use glium::{glutin,Surface};
 
-    // 1 . Creating an eventsLoop for handling window and device events
     let mut events_loop = glutin::EventsLoop::new();
-
-    // 2 . Specify Window parameters using glium::glutin::WindowBuilder::new().
-    //     These are window-specific attributes that have nothing to do with
-    //     OpenGL.
     let window = glutin::WindowBuilder::new();
-
-    // 3 . Specify Context parameters using glium::glutin::ContextBuilder::new().
-    //     Here we specify OpenGL-specific attributes like multisampling or vsync.
     let context = glutin::ContextBuilder::new();
-
-    // 4.  Create the OpenGL window (in glium, this is the Display):
-    //     glium::Display::new(window, context, &events_loop).unwrap().
-    //     This builds a Display using the given window and context attributes,
-    //     and registers the window with the given events_loop.
     let display = glium::Display::new(window,context,&events_loop).unwrap();
 
-    // 5. Keep the window open till the close event fires
+    #[derive(Copy, Clone)]
+    struct Vertex{
+        position: [f32; 2],
+    }
+
+    implement_vertex!(Vertex, position);
+
+    let vertex1 = Vertex {position: [-0.5,-0.5]};
+    let vertex2 = Vertex {position: [0.0,0.5]};
+    let vertex3 = Vertex {position: [0.5,-0.25]};
+    let shape = vec![vertex1,vertex2,vertex3];
+
+    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+
+    let vertex_shader_src = r#"
+        #version 140
+
+        in vec2 position;
+
+        void main(){
+            gl_Position = vec4(position,0.0,1.0);
+        }
+    "#;
+
+    let fragment_shader_src = r#"
+        #version 140
+
+        out vec4 color;
+
+        void main(){
+            color = vec4(1.0,0.0,0.0,1.0);
+        }
+    "#;
+
+    let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
+
     let mut closed = false;
     while !closed{
-        // 6 . Produce a frame to draw
+
         let mut target = display.draw();
         target.clear_color(0.0,0.0,1.0,1.0);
+        target.draw(&vertex_buffer,&indices,&program,&glium::uniforms::EmptyUniforms,&Default::default()).unwrap();
         target.finish().unwrap();
 
-        // 7 . listing the events produced by application and waiting to be received
         events_loop.poll_events(|ev|{
             match ev {
                 glutin::Event::WindowEvent {event, ..} => match event {

be5b3e8bcf6e19075708eb3b661df70a6d3a7bbc
yago <yagt16@gmail.com>
2018-10-21 15:02:35 +0200

    Modificación de color del triangulo a una transicion de valores

--- src/main.rs
+++ src/main.rs
@@ -34,10 +34,12 @@ fn main() {
         #version 140
 
         in vec2 position;
+        out vec2 my_attr;
 
         uniform mat4 matrix;
 
         void main(){
+            my_attr = position;
             gl_Position = matrix * vec4(position,0.0,1.0);
         }
     "#;
@@ -45,10 +47,11 @@ fn main() {
     let fragment_shader_src = r#"
         #version 140
 
+        in vec2 my_attr;
         out vec4 color;
 
         void main(){
-            color = vec4(1.0,0.0,0.0,1.0);
+            color = vec4(my_attr,0.0,1.0);
         }
     "#;
 
